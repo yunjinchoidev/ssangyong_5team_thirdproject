4dde684ff17a37ceaf047df9839e798666629294
yunjinchoidev <yunjinchoidev@gmail.com>
2022-01-04 11:16:10 +0900

    2022-01-04
    최윤진
    필요없는 VO 객체

--- project3/src/main/java/member/MemberVO.java
+++ project3/src/main/java/member/MemberVO.java
@@ -1,73 +1,260 @@
 package member;
 
-import java.sql.Date;
+import java.util.Date;
 
 public class MemberVO {
-	private String id;
-	private String pwd;
-	private String name;
-	private String email;
-	private Date joinDate;
-	private String gender;
+	private int mKey;
+	private String mId;
+	private String mPass;
+	private String mName;
+	private String mEmail;
+	private int mReg;
+	private char mGender;
+	private int mManiaQuot;
+	private Date mJoinDate;
+	private int mAuthNum;
+	private int mCash;
+	private int mPoint;
+	private int mLevel;
+	private char mEmailWhether;
+	private String mAddress;
+	private char mState;
+	private String mBlockList;
+	private double mSellerRate;
+	private int mGiftLevel;
+	private char mUnderFifteen;
+	private char mGiftDoneExist;
+	private int mGiftDoneCnt;
+	private String mMyGift;
+	private String mIp;
+	
 	public MemberVO() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public MemberVO(String id, String pwd, String name, String email, Date joinDate, String gender) {
+
+	public MemberVO(int mKey, String mId, String mPass, String mName, String mEmail, int mReg, char mGender,
+			int mManiaQuot, Date mJoinDate, int mAuthNum, int mCash, int mPoint, int mLevel, char mEmailWhether,
+			String mAddress, char mState, String mBlockList, double mSellerRate, int mGiftLevel, char mUnderFifteen,
+			char mGiftDoneExist, int mGiftDoneCnt, String mMyGift, String mIp) {
 		super();
-		this.id = id;
-		this.pwd = pwd;
-		this.name = name;
-		this.email = email;
-		this.joinDate = joinDate;
-		this.gender = gender;
-	}
-	public MemberVO(String id, String pwd, String name, String email, String gender) {
-		super();
-		this.id = id;
-		this.pwd = pwd;
-		this.name = name;
-		this.email = email;
-		this.gender = gender;
-	}
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getPwd() {
-		return pwd;
-	}
-	public void setPwd(String pwd) {
-		this.pwd = pwd;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public Date getJoinDate() {
-		return joinDate;
-	}
-	public void setJoinDate(Date joinDate) {
-		this.joinDate = joinDate;
-	}
-	public String getGender() {
-		return gender;
-	}
-	public void setGender(String gender) {
-		this.gender = gender;
+		this.mKey = mKey;
+		this.mId = mId;
+		this.mPass = mPass;
+		this.mName = mName;
+		this.mEmail = mEmail;
+		this.mReg = mReg;
+		this.mGender = mGender;
+		this.mManiaQuot = mManiaQuot;
+		this.mJoinDate = mJoinDate;
+		this.mAuthNum = mAuthNum;
+		this.mCash = mCash;
+		this.mPoint = mPoint;
+		this.mLevel = mLevel;
+		this.mEmailWhether = mEmailWhether;
+		this.mAddress = mAddress;
+		this.mState = mState;
+		this.mBlockList = mBlockList;
+		this.mSellerRate = mSellerRate;
+		this.mGiftLevel = mGiftLevel;
+		this.mUnderFifteen = mUnderFifteen;
+		this.mGiftDoneExist = mGiftDoneExist;
+		this.mGiftDoneCnt = mGiftDoneCnt;
+		this.mMyGift = mMyGift;
+		this.mIp = mIp;
 	}
 
-	
-	
+	public int getmKey() {
+		return mKey;
+	}
+
+	public void setmKey(int mKey) {
+		this.mKey = mKey;
+	}
+
+	public String getmId() {
+		return mId;
+	}
+
+	public void setmId(String mId) {
+		this.mId = mId;
+	}
+
+	public String getmPass() {
+		return mPass;
+	}
+
+	public void setmPass(String mPass) {
+		this.mPass = mPass;
+	}
+
+	public String getmName() {
+		return mName;
+	}
+
+	public void setmName(String mName) {
+		this.mName = mName;
+	}
+
+	public String getmEmail() {
+		return mEmail;
+	}
+
+	public void setmEmail(String mEmail) {
+		this.mEmail = mEmail;
+	}
+
+	public int getmReg() {
+		return mReg;
+	}
+
+	public void setmReg(int mReg) {
+		this.mReg = mReg;
+	}
+
+	public char getmGender() {
+		return mGender;
+	}
+
+	public void setmGender(char mGender) {
+		this.mGender = mGender;
+	}
+
+	public int getmManiaQuot() {
+		return mManiaQuot;
+	}
+
+	public void setmManiaQuot(int mManiaQuot) {
+		this.mManiaQuot = mManiaQuot;
+	}
+
+	public Date getmJoinDate() {
+		return mJoinDate;
+	}
+
+	public void setmJoinDate(Date mJoinDate) {
+		this.mJoinDate = mJoinDate;
+	}
+
+	public int getmAuthNum() {
+		return mAuthNum;
+	}
+
+	public void setmAuthNum(int mAuthNum) {
+		this.mAuthNum = mAuthNum;
+	}
+
+	public int getmCash() {
+		return mCash;
+	}
+
+	public void setmCash(int mCash) {
+		this.mCash = mCash;
+	}
+
+	public int getmPoint() {
+		return mPoint;
+	}
+
+	public void setmPoint(int mPoint) {
+		this.mPoint = mPoint;
+	}
+
+	public int getmLevel() {
+		return mLevel;
+	}
+
+	public void setmLevel(int mLevel) {
+		this.mLevel = mLevel;
+	}
+
+	public char getmEmailWhether() {
+		return mEmailWhether;
+	}
+
+	public void setmEmailWhether(char mEmailWhether) {
+		this.mEmailWhether = mEmailWhether;
+	}
+
+	public String getmAddress() {
+		return mAddress;
+	}
+
+	public void setmAddress(String mAddress) {
+		this.mAddress = mAddress;
+	}
+
+	public char getmState() {
+		return mState;
+	}
+
+	public void setmState(char mState) {
+		this.mState = mState;
+	}
+
+	public String getmBlockList() {
+		return mBlockList;
+	}
+
+	public void setmBlockList(String mBlockList) {
+		this.mBlockList = mBlockList;
+	}
+
+	public double getmSellerRate() {
+		return mSellerRate;
+	}
+
+	public void setmSellerRate(double mSellerRate) {
+		this.mSellerRate = mSellerRate;
+	}
+
+	public int getmGiftLevel() {
+		return mGiftLevel;
+	}
+
+	public void setmGiftLevel(int mGiftLevel) {
+		this.mGiftLevel = mGiftLevel;
+	}
+
+	public char getmUnderFifteen() {
+		return mUnderFifteen;
+	}
+
+	public void setmUnderFifteen(char mUnderFifteen) {
+		this.mUnderFifteen = mUnderFifteen;
+	}
+
+	public char getmGiftDoneExist() {
+		return mGiftDoneExist;
+	}
+
+	public void setmGiftDoneExist(char mGiftDoneExist) {
+		this.mGiftDoneExist = mGiftDoneExist;
+	}
+
+	public int getmGiftDoneCnt() {
+		return mGiftDoneCnt;
+	}
+
+	public void setmGiftDoneCnt(int mGiftDoneCnt) {
+		this.mGiftDoneCnt = mGiftDoneCnt;
+	}
+
+	public String getmMyGift() {
+		return mMyGift;
+	}
+
+	public void setmMyGift(String mMyGift) {
+		this.mMyGift = mMyGift;
+	}
+
+	public String getmIp() {
+		return mIp;
+	}
+
+	public void setmIp(String mIp) {
+		this.mIp = mIp;
+	}
+		
 	
 }
